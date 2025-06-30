e89c6d7476b2ff507f39ef701f5cb9ef07dad572
edwinnelly <112112833+edwinnelly@users.noreply.github.com>
2025-06-30 11:11:23 +0100

    updated the headerb and the footer

---
+++ inc/footer.php
@@ -0,0 +1,51 @@
+<footer>
+        <div class="footer container">
+            <div class="top">
+                <div class="each">
+                    <img src="assets/10001.png" alt="" srcset="" style="width: 100px;">
+                </div>
+
+                <div class="each">
+                    <p class="my-0 fw-bold" style="font-size: 13px; color: gray;">Products</p>
+
+                    <p class="my-0">Products</p>
+                    <p class="my-0">Products</p>
+                    <p class="my-0">Products</p>
+                    <p class="my-0">Products</p>
+                    <p class="my-0">Products</p>
+                    <p class="my-0">Products</p>
+                </div>
+                <div class="each">
+                    <p class="my-0 fw-bold" style="font-size: 13px; color: gray;">Resources</p>
+
+                    <p class="my-0">Plan Cost Calculator</p>
+                    <p class="my-0">Plan Cost Calculator</p>
+                    <p class="my-0">Products</p>
+                    <p class="my-0">Plan Cost Calculator</p>
+                    <p class="my-0">Products</p>
+                    <p class="my-0">Products</p>
+                </div>
+                <div class="each">
+                    <p class="my-0 fw-bold" style="font-size: 13px; color: gray;">Company</p>
+
+                    <p class="my-0">Products</p>
+                    <p class="my-0">Products</p>
+                    <p class="my-0">Plan Cost Calculator</p>
+                    <p class="my-0">Plan Cost Calculators</p>
+                    <p class="my-0">Products</p>
+                    <p class="my-0">Products</p>
+                </div>
+            </div>
+
+            <div class="bottom">
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-linkedin"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <p class="my-0">Privacy</p>
+                <p class="my-0">Terms and Conditions of Use</p>
+                <p class="my-0">Legal Disclosures</p>
+                <p class="my-0">Your Privacy Choices</p>
+            </div>
+        </div>
+    </footer>
